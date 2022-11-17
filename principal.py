ae7614f8bede808f3d7fee7f97617500ba7d0408
Tulioceltic <72052033+Tulioceltic@users.noreply.github.com>
2022-11-17 17:49:52 -0500

    Add files via upload

--- principal.py
+++ principal.py
@@ -6,20 +6,13 @@ from flask import Flask, render_template, redirect, request, url_for
 app = Flask(__name__)
 
 ordenb = [
-    ['A1','A2','B3'],
-    ['B1','B2','B3'],
-    ['C1','C2','C3'],
-    ['D1','D2','D3'],    
+    {'A1':True,'A2':False,'A3':True},
+    {'B1':False,'B2':True,'B3':True},
+    {'C1':True,'C2':True,'C3':True},
+    {'D1':True,'D2':True,'D3':False},    
 ]
 
 heib = len(ordenb)
-
-habilitadob = [
-    [True,False,True],
-    ['B1','B2','B3'],
-    ['C1','C2','C3'],
-    ['D1','D2','D3'],    
-]
 
 
 @app.route('/', methods = ['POST','GET'])
@@ -28,12 +21,12 @@ def inicio():
 
 @app.route('/cu', methods = ['POST','GET'])
 def ver_cuadrado():
-    return render_template('cu.html',ordenf=ordenb, habilitadof=habilitadob, heif=heib)
+    return render_template('cu.html',ordenf=ordenb, heif=heib)
 
 
 @app.route('/trapecio', methods = ['POST','GET'])
-def ver_circulo():
-    return render_template('trapecio.html',ordenf=ordenb)
+def ver_trap():
+    return render_template('trapecio.html',ordenf=ordenb,heif=heib)
 
 if __name__ == '__main__':
     app.run(debug=True)
